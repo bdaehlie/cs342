10e1117f4f977c1be92489bc0bb667fbb1751982
Josh Aas <jaas@kflag.net>
2016-03-29 00:21:30 -0500

    Add initial locking jobs program.

---
+++ locking/locking-jobs.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+// gcc locking-jobs.c -g -o locking-jobs
+
+// Tips
+//
+// You can direct output from this program to a file like this:
+//
+// $ ./locking-jobs > output.txt
+//
+// Then you can look for duplicate lines in the file like this:
+//
+// $ cat output.txt | uniq -d
+
+#define JOB_COUNT 1000000
+
+uint64_t jobs_completed = 0;
+
+void do_job(uint64_t job_id) {
+  for (int i = 0; i < 5; i++) {
+    asm("nop");
+  }
+  printf("Completed job #%lld\n", job_id);
+}
+
+void *do_jobs(void *thread_id)
+{
+  printf("Thread #%p reporting for duty!\n", thread_id);
+
+  while (jobs_completed < JOB_COUNT) {
+    do_job(jobs_completed + 1);
+    jobs_completed++;
+  }
+
+  pthread_exit(NULL);
+}
+
+int main(int argc, char **argv) {
+  for (long i = 0; i < 2; i++) {
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, do_jobs, (void*)i) != 0) {
+      printf("Thread creation error!\n");
+    }
+    pthread_detach(thread);
+  }
+
+  pthread_exit(NULL);
+
+  return 0;
+}
